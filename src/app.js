f6ab7b11591c9fe8d4625a4c8250cf7a1ca5157f
Rama <ramiroquesadaperez@gmail.com>
2023-03-16 04:18:47 -0300

    Llamada a la api y funcion para obtener ubicacion del usuario creadas

--- src/app.js
+++ src/app.js
@@ -1,2 +1,50 @@
 import "./reset.css";
 import "./style.css";
+
+function getWeather(location){
+    const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=`;
+
+    const apiKey = "37b5cee3414c41022c728c8f21d250e3";
+
+    const appIdText = "&appid="
+
+    const apiOptional = "&lang=es&units=metric";
+
+    fetch(apiCall + location + appIdText + apiKey + apiOptional)
+    .then (response => response.json())
+    .then (data => {
+        console.log(data)
+        renderData(data);
+    })   
+    .catch((error)=> console.log(error))
+}
+
+
+
+let renderData = (data) =>{
+    
+}
+
+
+getWeather("Mercedes, UY");
+
+const getUserPosition = ()=>{
+
+const successCallback = (position) => {
+    console.log(position);
+  };
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+  
+  const options = {
+    enableHighAccuracy: true
+    
+  };
+
+const userLocation = navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+console.log(userLocation)
+}
+
+
